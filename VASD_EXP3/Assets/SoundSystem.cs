69d80fcae2aae62a8acfc4fd0409cd45546798a5
Stijn de Koning <sdk.stijn@gmail.com>
2020-03-10 20:25:34 +0100

    updated soundsystem includes

--- VASD_EXP3/Assets/SoundSystem.cs
+++ VASD_EXP3/Assets/SoundSystem.cs
@@ -1,11 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System;
 using UnityEngine;
 using FMOD;
-using FMODUnity;
-
 
 public class SoundSystem : MonoBehaviour
 {
