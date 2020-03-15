11e14a55d7ed1a8a3c089d64b0ff41dd360d2e94
Stijn de Koning <sdk.stijn@gmail.com>
2020-03-15 20:57:44 +0100

    implemented playloop, user input and list parsing

--- VASD_EXP3/Assets/SoundSystem.cs
+++ VASD_EXP3/Assets/SoundSystem.cs
@@ -2,20 +2,31 @@
 using System.IO;
 using UnityEngine;
 using FMOD;
+using System;
+using System.Linq;
 
 public class SoundSystem : MonoBehaviour
 {
-    public List<string> fileNames; // list containing al wav files
-
+    // list containing al wav files
+    public List<string> fileNames;
+    public List<List<string>> entryList = new List<List<string>>();
+    // FMOD core API
     public FMOD.System corSystem;
     public ChannelGroup channelgroup;
     public Channel channel;
+    // playing bool
+    private bool playing;
+    // string input
+    private float bpm;
+    public UnityEngine.UI.InputField inputField;
+    public UnityEngine.UI.InputField inputField2;
 
-    public bool playing = false;
+    bool startPlaying;
+    float loopTime;
 
     void Start()
     {
-        // acced FMOD
+        // acces FMOD
         corSystem = FMODUnity.RuntimeManager.CoreSystem;
         uint version;
         corSystem.getVersion(out version);
@@ -25,35 +36,51 @@ public class SoundSystem : MonoBehaviour
 
         // load all files
         ReadFiles();
-        print("Loaded files: ");
-        for (int i = 0; i < fileNames.Count; i++)
-        {
-            print(fileNames[i]);
-        }
-
-        // TODO: handle user input
-        int bpm = calculateTime();
-
-
     }
+
 
     void Update()
     {
-        // TODO: run gameplay loop, statemachine
-
-        // TODO: start the game loop
-        gameLoop();
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            startPlaying = true;
+        }
+        if (startPlaying == true)
+        {
+            gameLoop();
+        }
     }
 
     public void ReadFiles()
     {
         DirectoryInfo dir = new DirectoryInfo("../BounceLocation");
         FileInfo[] info = dir.GetFiles("*.wav*");
+
+        int oldValue = 0;
+        int layerAmount = 0;
+
         foreach (FileInfo f in info)
         {
             fileNames.Add(f.Name);
-            // TODO: put into a entry list and divide by layer
-            //entryList.append([name, name[4], possibleTransitions, name[6]])
+            // get layerAmount
+            if (int.Parse(f.Name[0].ToString()) > oldValue)
+            {
+                layerAmount = int.Parse(f.Name[0].ToString());
+            }
+        }
+
+        // initialise entrylist
+        for (int i = 0; i < layerAmount; i++)
+        {
+            entryList.Add(new List<string>());
+        }
+
+        // parse list
+        print("loaded files: ");
+        for (int i = 0; i < fileNames.Count; i++)
+        {
+            entryList[int.Parse(fileNames[i][0].ToString()) - 1].Add(fileNames[i]);
+            print(fileNames[i]);
         }
     }
 
@@ -66,33 +93,35 @@ public class SoundSystem : MonoBehaviour
         print("playing: " + filename);
     }
 
-    private int calculateTime()
+    private float calculateTime(float beatsPerMinute, float beatAmount)
     {
-        // get user input on bpm and amount of beats
-        return 0;
+        float duration = (60 / beatsPerMinute) * beatAmount;
+        return duration;
     }
 
     private void gameLoop()
     {
+        // Get UI information (dit hooeft niet in update)
+        bpm = calculateTime(float.Parse(inputField.text), float.Parse(inputField2.text));
+
         // check if needs to stop
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            print("exit gameLoop");
+            print("paused");
+            // TODO: actually stop the audio 
         }
 
         if (playing == false)
         {
-            playTrack(fileNames[0]);
-            playTrack(fileNames[3]);
+            // TODO: implement transition decision system
+            loopTime = Time.time + bpm;
+            playTrack(entryList[0][0]);
+            playTrack(entryList[1][0]);
             playing = true;
         }
-
-        // if a certain time has passed: paying = false
-
-        // check for pause or stop
-        // For every layer:
-        //  play current files
-        //  decide what next files can be played
-        //  wait for the horizontal loops to finish and run again
+        if (Time.time >= loopTime)
+        {
+            playing = false;
+        }
     }
 }
