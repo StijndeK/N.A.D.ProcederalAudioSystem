8bd114162ab1b5542516c9dcff4f800cbdb2e3ee
Stijn de Koning <sdk.stijn@gmail.com>
2020-04-04 11:06:25 +0200

    new dynamic component system and some added functionalities

---
+++ VASD_EXP5/Assets/Scripts/SequencerSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SequencerSystem : MonoBehaviour
+{
+    // Parameters
+    public Slider parameterSettingSlider;
+    public List<int> parameterX = new List<int>();
+    // line position
+    public Transform lineTransform;
+
+    public void getLayerParameter(int layer, float position)
+    {
+        if (parameterX.Count < layer)
+        {
+            parameterX.Add(0);
+        }
+        parameterX[layer - 1] = (position >= lineTransform.position[0]) ? 1 : 0;
+    }
+
+    // TODO: button for setting new lines and lengthenig slider
+}
